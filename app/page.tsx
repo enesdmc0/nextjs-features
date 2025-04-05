c645643c9f3f9c3a54463b30727be09658ce6db9
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2025-04-05 14:37:23 +0300

    10: feat: implement address form with validation and submission logic
    - Added address form with fields for street address, apartment, city, state, zip code, and country.
    - Integrated Zod for form validation and error handling.
    - Created action to handle form submission and simulate network delay.
    - Implemented UI components for form layout using Tailwind CSS.
    - Added success and error notifications using Sonner.
    - Updated package.json with new dependencies for form handling and UI components.
    - Created utility functions for date formatting and class name merging.
    - Established Prisma schema for Todo model and integrated MongoDB as the database.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { TodoList } from "@/components/todo-list";
+import { NewTodoForm } from "@/components/new-todo-form";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex items-center justify-center flex-col min-h-screen ">
+      <div className="container max-w-2xl">
+        <h1 className="font-serif text-3xl font-medium">Keep track of your tasks</h1>
+        <NewTodoForm />
+        <TodoList />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ lib/utils.ts
@@ -0,0 +1,17 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+export function formatDate(date: Date) {
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })
+  return formatter.format(date)
+}
+
